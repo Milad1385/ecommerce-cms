6d5001a4cb798be5c9f7ed90f2a4a4cddc164b0f
Milad salamian <miladsalami1385@gmail.com>
2024-06-20 04:17:26 -0700

    init project

---
+++ src/router/routes.tsx
@@ -0,0 +1,175 @@
+import { lazy } from 'react';
+import Articles from '../pages/Pages/Articles';
+import Products from '../pages/Pages/Products';
+import Categories from '../pages/Pages/Categories';
+import Slider from '../pages/Pages/Slider';
+import Order from '../pages/Pages/Order';
+import LoginBoxed from '../pages/Authentication/LoginBoxed';
+const Index = lazy(() => import('../pages/Index'));
+
+const Tables = lazy(() => import('../pages/Tables'));
+const Basic = lazy(() => import('../pages/DataTables/Basic'));
+const Advanced = lazy(() => import('../pages/DataTables/Advanced'));
+const Skin = lazy(() => import('../pages/DataTables/Skin'));
+const OrderSorting = lazy(() => import('../pages/DataTables/OrderSorting'));
+const MultiColumn = lazy(() => import('../pages/DataTables/MultiColumn'));
+const MultipleTables = lazy(() => import('../pages/DataTables/MultipleTables'));
+const AltPagination = lazy(() => import('../pages/DataTables/AltPagination'));
+const Checkbox = lazy(() => import('../pages/DataTables/Checkbox'));
+const RangeSearch = lazy(() => import('../pages/DataTables/RangeSearch'));
+const Export = lazy(() => import('../pages/DataTables/Export'));
+const ColumnChooser = lazy(() => import('../pages/DataTables/ColumnChooser'));
+const Profile = lazy(() => import('../pages/Users/Profile'));
+const AccountSetting = lazy(() => import('../pages/Users/AccountSetting'));
+
+const RegisterBoxed = lazy(() => import('../pages/Authentication/RegisterBoxed'));
+const UnlockBoxed = lazy(() => import('../pages/Authentication/UnlockBox'));
+const RecoverIdBoxed = lazy(() => import('../pages/Authentication/RecoverIdBox'));
+const LoginCover = lazy(() => import('../pages/Authentication/LoginCover'));
+const RegisterCover = lazy(() => import('../pages/Authentication/RegisterCover'));
+const RecoverIdCover = lazy(() => import('../pages/Authentication/RecoverIdCover'));
+const UnlockCover = lazy(() => import('../pages/Authentication/UnlockCover'));
+const About = lazy(() => import('../pages/About'));
+const Error = lazy(() => import('../components/Error'));
+
+const routes = [
+    // dashboard
+    {
+        path: '/',
+        element: <Index />,
+    },
+
+    //  Tables page
+    {
+        path: '/tables',
+        element: <Tables />,
+    },
+    // Data Tables
+    {
+        path: '/datatables/basic',
+        element: <Basic />,
+    },
+    {
+        path: '/datatables/advanced',
+        element: <Advanced />,
+    },
+    {
+        path: '/datatables/skin',
+        element: <Skin />,
+    },
+    {
+        path: '/datatables/order-sorting',
+        element: <OrderSorting />,
+    },
+    {
+        path: '/datatables/multi-column',
+        element: <MultiColumn />,
+    },
+    {
+        path: '/datatables/multiple-tables',
+        element: <MultipleTables />,
+    },
+    {
+        path: '/datatables/alt-pagination',
+        element: <AltPagination />,
+    },
+    {
+        path: '/datatables/checkbox',
+        element: <Checkbox />,
+    },
+    {
+        path: '/datatables/range-search',
+        element: <RangeSearch />,
+    },
+    {
+        path: '/datatables/export',
+        element: <Export />,
+    },
+    {
+        path: '/datatables/column-chooser',
+        element: <ColumnChooser />,
+    },
+    // Users page
+    {
+        path: '/users/profile',
+        element: <Profile />,
+    },
+    {
+        path: '/users/user-account-settings',
+        element: <AccountSetting />,
+    },
+
+    //Authentication
+    {
+        path: '/auth/boxed-signin',
+        element: <LoginBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/boxed-signup',
+        element: <RegisterBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/boxed-lockscreen',
+        element: <UnlockBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/boxed-password-reset',
+        element: <RecoverIdBoxed />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/cover-login',
+        element: <LoginCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/cover-register',
+        element: <RegisterCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/cover-lockscreen',
+        element: <UnlockCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/auth/cover-password-reset',
+        element: <RecoverIdCover />,
+        layout: 'blank',
+    },
+    {
+        path: '/about',
+        element: <About />,
+    },
+    {
+        path: '/articles',
+        element: <Articles />,
+    },
+    {
+        path: '/products',
+        element: <Products />,
+    },
+    {
+        path: '/categories',
+        element: <Categories />,
+    },
+    {
+        path: '/slider',
+        element: <Slider />,
+    },
+    {
+        path: '/orders',
+        element: <Order />,
+    },
+
+    {
+        path: '*',
+        element: <Error />,
+        layout: 'blank',
+    },
+];
+
+export { routes };
